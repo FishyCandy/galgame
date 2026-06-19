d02d4be61fc18d3b855c07a838f06674a9c0f59d
FishyCandy <2971525240@qq.com>
2026-06-19 19:43:20 +0800

    fix: 台词回顾字体与对话框一致(角色名26f/台词22f)

--- src/main/java/org/galgame/LogPanel.java
+++ src/main/java/org/galgame/LogPanel.java
@@ -24,7 +24,7 @@ public class LogPanel extends JPanel {
         this.gamePanel = gamePanel;
         this.history = new ArrayList<>(history); // 防御性拷贝
         this.dialogFont = dialogFont;
-        this.nameFont = dialogFont.deriveFont(Font.BOLD, dialogFont.getSize2D());
+        this.nameFont = dialogFont.deriveFont(26f);
 
         setLayout(new BorderLayout());
         setOpaque(false);
@@ -127,7 +127,7 @@ public class LogPanel extends JPanel {
         dialogueBox.setBorder(BorderFactory.createEmptyBorder(8, 16, 8, 16));
 
         JLabel textLabel = new JLabel("<html><body style='width:550px'>" + escapeHtml(text) + "</body></html>");
-        textLabel.setFont(dialogFont);
+        textLabel.setFont(dialogFont.deriveFont(22f));
         textLabel.setForeground(Color.WHITE);
         textLabel.setVerticalAlignment(JLabel.TOP);
         dialogueBox.add(textLabel, BorderLayout.CENTER);
