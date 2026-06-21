47e625497f74357e33e88c3ec877d0b6e3a5e530
FishyCandy <2971525240@qq.com>
2026-06-21 19:47:32 +0800

    fix: 游戏设置覆盖层透明度改为230

--- src/main/java/org/galgame/SettingsPanel.java
+++ src/main/java/org/galgame/SettingsPanel.java
@@ -281,7 +281,7 @@ public class SettingsPanel extends JPanel {
             }
             g2.drawImage(bgImage, drawX, drawY, drawW, drawH, this);
             // 半透明黑色覆盖层，用于视觉区分背景和设置内容
-            g2.setColor(new Color(0, 0, 0, 200));
+            g2.setColor(new Color(0, 0, 0, 230));
             g2.fillRect(0, 0, getWidth(), getHeight());
         } else {
             GradientPaint gp = new GradientPaint(0, 0, new Color(30, 30, 60),
