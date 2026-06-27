9c141fe9c13f824dfaa7eb510643d34579d7426f
FishyCandy <2971525240@qq.com>
2026-06-27 10:35:35 +0800

    撤销进度条音量条隐藏，恢复完全可见

--- src/main/java/org/galgame/MusicPlayerPanel.java
+++ src/main/java/org/galgame/MusicPlayerPanel.java
@@ -225,7 +225,6 @@ public class MusicPlayerPanel extends JPanel {
         progressSlider = new JSlider(0, 1000, 0);
         progressSlider.setOpaque(false);
         progressSlider.setFocusable(false);
-        progressSlider.setVisible(false);
         progressSlider.addChangeListener(e -> {
             if (programmaticSliderUpdate) return;
             if (progressSlider.getValueIsAdjusting()) {
@@ -249,7 +248,6 @@ public class MusicPlayerPanel extends JPanel {
         volumeSlider = new JSlider(0, 100, 80);
         volumeSlider.setOpaque(false);
         volumeSlider.setFocusable(false);
-        volumeSlider.setVisible(false);
         volumeSlider.addChangeListener(e -> {
             double vol = volumeSlider.getValue() / 100.0;
             MusicPlayer.setGlobalVolume(vol);
